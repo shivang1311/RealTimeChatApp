721a822ceaabdd8849cccedb6b39d2161be01e69
shivang1311 <shivangag131104@gmail.com>
2025-06-24 22:55:39 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
+
+let chatHistory = [];
+let onlineUsers = {};
+
+io.on('connection', (socket) => {
+  
+  socket.on('login', (username) => {
+    socket.username = username;
+    onlineUsers[username] = socket.id;
+    io.emit('onlineUsers', Object.keys(onlineUsers));
+    socket.emit('chatHistory', chatHistory);
+  });
+
+  socket.on('sendMessage', (msg) => {
+    chatHistory.push(msg);
+    io.emit('receiveMessage', msg);
+  });
+
+  socket.on('typing', (username) => {
+    io.emit('typing', username);
+  });
+  socket.on('stop_typing', (username) => {
+    io.emit('stop_typing', username);
+  });
+
+  socket.on('disconnect', () => {
+    if (socket.username) {
+      delete onlineUsers[socket.username];
+      io.emit('onlineUsers', Object.keys(onlineUsers));
+    }
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('Backend server is running!');
+});
+
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
